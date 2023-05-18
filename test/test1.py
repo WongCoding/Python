82791373fa74be4a00ed618737200718143ba039
wang_fc <13719224900@163.com>
2023-05-18 11:26:52 +0800

    test10101010

---
+++ test/test1.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from time import sleep
+
+url1 = 'https://cn.bing.com/'
+driver = webdriver.Chrome()
+
+driver.get(url1)
+sleep(1)
+driver.maximize_window()
+driver.find_element_by_xpath('//*[@id="sb_form_q"]').send_keys("oppo解散芯片研发团队")
+driver.find_element_by_xpath('//*[@id="search_icon"]').click()
+
+sleep(3)
+driver.find_element_by_xpath('//*[@id="sb_form_q"]').clear()
+driver.find_element_by_xpath('//*[@id="sb_form_q"]').send_keys("公积金贷款条件")
+driver.find_element_by_xpath('//*[@id="search_icon"]').click()
+sleep(3)
+driver.back()
+sleep(1)
+
+driver.close()
